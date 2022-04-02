f8a426650b2d6cabf76eca6d3c891e09057afb99
LianNidam <liannidam04@gmail.com>
2022-04-02 16:46:07 +0300

    The name, last name and email are sent successfully

--- app.js
+++ app.js
@@ -8,10 +8,21 @@ const request =require("request");
 const app= express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req,res){
   res.sendFile(__dirname + "/signup.html");
 });
+
+app.post("/", function(req, res){
+  var firstName= req.body.fName;
+  var lasttName= req.body.lName;
+  var email= req.body.email;
+
+  console.log(firstName, lasttName, email);
+
+});
+
 
 app.listen(3000, function(){
   console.log("server is runnig on port 3000.")
